1c2864e40ee610d6fa5c6e882dc5bfc8f7fb1db9
Daniel Mendoza <duartixed@hotmail.com>
2025-03-16 20:41:57 -0500

    Subiendo Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,55 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js', // 🔹 Cambiado de main.js a index.js
-    style: './src/assets/styles.css',
-  },
+const config = {
+  mode: 'development', // Cambia a 'production' si es para producción
+
+  entry: './frontend/src/index.js', // Se eliminó 'style' y ahora se importa en index.js
+
   devtool: 'inline-source-map',
+
   devServer: {
-    static: './dist',
+    static: path.resolve(__dirname, 'frontend/public'),
     hot: true,
     liveReload: true,
+    historyApiFallback: true, // Redirige todas las rutas a index.html
+    port: 3000, // Puedes cambiar el puerto si es necesario
   },
+
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './frontend/public/index.html',
+      inject: 'body',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css',
     }),
   ],
+
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/, // Se agregó soporte para JSX (React)
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'], // Se agregó soporte para React
+          },
+        },
+      },
+      {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -37,18 +59,31 @@ module.exports = {
         },
       },
       {
-        test: /\.(js|jsx)$/, // Soporte para React (JS y JSX)
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+          sources: {
+            list: [
+              {
+                tag: 'img',
+                attribute: 'src',
+                type: 'src',
+              },
+            ],
+          },
         },
       },
     ],
   },
+
   optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
     runtimeChunk: 'single',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'], // Permite importar archivos sin escribir la extensión
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
   },
 };
+
+module.exports = config;
